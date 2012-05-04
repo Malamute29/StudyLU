c5a5f1681bbe0ca0159bb2aae516fb8cac9800e4
Spenser Steele <spenser.steele@gmail.com>
2012-05-04 13:49:30 -0500

    fixed title of user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @title = user.f_name + " " + user.l_name  # might not work
+    @title = @user.username  # Should change to full name
 
     respond_to do |format|                  # This is not in the book
       format.html # show.html.erb           # This is not in the book
