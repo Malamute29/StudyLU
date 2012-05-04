8965e4613f0e47551dd81e79614c7b0f59cd8701
Spenser Steele <spenser.steele@gmail.com>
2012-05-04 04:55:17 -0500

    updated user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,8 +24,14 @@ class User < ActiveRecord::Base
     before_save :encrypt_password
     
     def has_password?(submitted_password)
+      encrypted_password == encrypt(submitted_password)
     end
     
+    def self.authenticate(email, submitted_password)
+      user = find_by_email(email)   # I think this means we need to use email to login
+      return nil if user.nil?
+      return user if user.has_password?(submitted_password)
+    end
         
     private
     def encrypt_password
