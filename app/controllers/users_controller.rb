e2bebc52c55183373f2b63f1a592af75b4148527
Spenser Steele <spenser.steele@gmail.com>
2012-05-04 13:18:35 -0500

    moved some stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
+    @title = user.f_name + " " + user.l_name  # might not work
 
     respond_to do |format|                  # This is not in the book
       format.html # show.html.erb           # This is not in the book
