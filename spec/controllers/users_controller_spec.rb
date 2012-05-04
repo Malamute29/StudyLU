a9c3a1ab0d36f52fd73abba4a1bdceae83ad7d30
Spenser Steele <spenser.steele@gmail.com>
2012-05-04 11:37:51 -0500

    updated users_controller_spec.rb

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -15,4 +15,21 @@ describe UsersController do
       response.should have_selector("title", :content => "Sign up")
     end
   end
+  
+  describe "GET 'show'" do
+    
+    before(:each) do
+      @user = Factory(:user)
+    end
+    
+    it "should be successful" do
+      get :show, :id => @user
+      response.shold be_success
+    end
+    
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user):should == @user
+    end
+  end
 end
