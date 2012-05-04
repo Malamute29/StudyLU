bddfe700d2fb717788fac94cbbea0b300cf0bea3
Spenser Steele <spenser.steele@gmail.com>
2012-05-04 10:31:22 -0500

    commented on 'show'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,9 +20,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user }
+    respond_to do |format|                  # This is not in the book
+      format.html # show.html.erb           # This is not in the book
+      format.json { render json: @user }    # This is not in the book
     end
   end
 
